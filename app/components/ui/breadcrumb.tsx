fbae7ae3ef54a7897b77ea09c5d0c901775cfe43
Robert Eggl <robert@eggl.dev>
2025-12-04 12:17:54 +0100

    chore(deps): update @biomejs/biome and related CLI packages

--- app/components/ui/breadcrumb.tsx
+++ app/components/ui/breadcrumb.tsx
@@ -55,6 +55,7 @@ function BreadcrumbLink({
 function BreadcrumbPage({ className, ...props }: React.ComponentProps<'span'>) {
 	return (
 		// biome-ignore lint/a11y/useFocusableInteractive: TODO
+		// biome-ignore lint/a11y/useSemanticElements: TODO
 		<span
 			data-slot="breadcrumb-page"
 			role="link"
